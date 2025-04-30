3820407f36cb1251df949ea10ea87bb23391c494
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-04-30 09:44:06 +0100

    javascript done

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 const apiKey= "e771f2e68b30a9e4546a70a63ed50bf9";
+
+async function getWeather() {
+    const city = document.getElementById("city-input").value.trim();
+    const weatherBox = document.getElementById("weather-box");
+  
+    if (!city) {
+      alert("Escreve o nome da cidade!");
+      return;
+    }
+  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&units=metric&lang=pt&appid=${apiKey}`;
+  
+    try {
+      const res = await fetch(url);
+      if (!res.ok) throw new Error("Cidade não encontrada!");
+  
+      const data = await res.json();
+  
+      const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  
+      weatherBox.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <img src="${icon}" alt="Ícone clima">
+        <p><strong>${Math.round(data.main.temp)}°C</strong> - ${data.weather[0].description}</p>
+      `;
+    } catch (error) {
+      weatherBox.innerHTML = `<p style="color: #f66;">Erro: ${error.message}</p>`;
+    }
+  }
